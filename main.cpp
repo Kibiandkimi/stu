f6e6753e0b8921566976d20ccd95e94dda6a89a5
kibi <kibiandkimi@outlook.com>
2022-09-10 13:59:38 +0800

    Revert "o2"
    This reverts commit b1946bd2d926771ca6ee41ea8f19516593db3a8b.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, a[100005];
-int l, r;
 
 void read(int &x);
 
@@ -9,16 +8,6 @@ int main(){
     read(n);
     for(int i = 1; i <= n; i++){
         read(a[i]);
-        if(!l && a[i]!=3){
-            l = i;
-        }
-        if(a[i]!=2){
-            r = i;
-        }
-    }
-    int ans = 0;
-    for(int i = l; i <= r; i++){
-
     }
 
 }
