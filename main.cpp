9f04b53dad2134fd01c179a30b5b50413ea5a5a8
kibi <kibiandkimi@outlook.com>
2022-09-10 13:51:21 +0800

    o1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, a[100005];
+
+void read(int &x);
+
+int main(){
+    read(n);
+    for(int i = 1; i <= n; i++){
+        read(a[i]);
+    }
+
+}
