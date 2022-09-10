b768ab78f2059dae1f7dc58a117e108e9845b5d6
kibi <kibiandkimi@outlook.com>
2022-09-10 18:02:09 +0800

    o9

--- T265252.cpp
+++ T265252.cpp
@@ -13,9 +13,27 @@ int main(){
         printf("1");
         return 0;
     }
-    long long x = f(b, a);
-    x = x&-x;
-    printf("%lld", x);
+    if(a <= 11 && b<=11 && c<=11) {
+        long long x = f(b, a);
+        x = x & -x;
+        printf("%lld", x);
+    }else{
+        long long t = c&-c;
+        if(a%2){
+            // printf("1");
+            c--;
+            t = c&-c;
+            long long x = a;
+            for(int i = 1; i <= 5; i++){
+                long long tem = (long long)pow(x, 2*c)^c;
+                x = tem&-tem;
+            }
+            t = min(x, t);
+            printf("%lld", t);
+        }else {
+            printf("%lld", t);
+        }
+    }
 }
 
 long long f(long long k, long long x){
