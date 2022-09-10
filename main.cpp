b1946bd2d926771ca6ee41ea8f19516593db3a8b
kibi <kibiandkimi@outlook.com>
2022-09-10 13:55:26 +0800

    o2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, a[100005];
+int l, r;
 
 void read(int &x);
 
@@ -8,6 +9,16 @@ int main(){
     read(n);
     for(int i = 1; i <= n; i++){
         read(a[i]);
+        if(!l && a[i]!=3){
+            l = i;
+        }
+        if(a[i]!=2){
+            r = i;
+        }
+    }
+    int ans = 0;
+    for(int i = l; i <= r; i++){
+
     }
 
 }
