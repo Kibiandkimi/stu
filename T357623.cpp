ea0e2ff30f8e279ebe395dda8c0f7155f13cfbd8
kibi <kibiandkimi@outlook.com>
2023-07-27 17:00:06 +0800

    T1 v2

--- T357623.cpp
+++ T357623.cpp
@@ -11,15 +11,17 @@ public:
 static House house[N*N+5];
 
 void read(long long&);
-void f(int);
 
-long long x, y, np, nt, res, ans, nc, nm;
+bool cmp(long long a, long long b){
+    return a > b;
+}
+
+long long ans;
 long long pw[75];
 
 int main(){
 
-    long long c, n, m, p, t; // c, 公寓数  n, 地图边长  m, 单次朋友个数  p, 体力上限  t, 天数
-    // t, 公寓上限  p, 单次距离上限
+    long long c, n, m, p, t;
     read(c), read(n), read(m), read(p), read(t);
     if(t == 0){
         printf("0\n");
@@ -33,71 +35,51 @@ int main(){
 
     static bool map[N+5][N+5];
 
-    int cnt = 0;
     for(int i = 1; i <= c; i++){
         long long _x, _y, g, w;
         read(_x), read(_y), read(g), read(w);
-        //        if(g > m){
-        //        	house[++cnt] = {_x, _y, m, w};
-        //        	house[++cnt] = {_x, _y, min(m, g-m), w};
-        //        }else{
-        //        	house[++cnt] = {_x, _y, g, w};
-        //        }
-        house[++cnt] = {_x, _y, g, w};
+        house[i] = {_x, _y, g, w};
         map[_x][_y] = true;
     }
-    np = p;
-    nc = c;
-    nm = m;
+
+    long long cnt;
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= n; j++){
             if(map[i][j]){
                 continue;
             }
-            x = i, y = j;
-            nt = t;
-            res = 0;
-            f(1);
-
+            static long long val[N*N*2+5];
+            cnt = 0;
+            for(int k = 1; k <= c; k++){
+                static long long dis;
+                dis = abs(house[k].x-i) + abs(house[k].y-j);
+                if(p >= house[k].w + pw[dis]){
+                    static long long mn;
+                    mn = min((p - pw[dis])/house[k].w, house[k].g);
+                    static long long num;
+                    num = min(mn, m);
+                    val[++cnt] = num;
+                    if(num < house[k].g){
+                        num = min(num, house[k].g - num);
+                        val[++cnt] = num;
+                    }
+                }
+            }
+            if(cnt){
+                static long long res;
+                res = 0;
+                sort(val+1, val+1+cnt, cmp);
+                long long mxk = min(t, cnt);
+                for(int k = 1; k <= mxk; k++){
+                    res += val[k];
+                }
+                ans = max(ans, res);
+            }
         }
     }
 
     printf("%lld\n", ans);
 
-}
-
-void f(int u){
-    // 去
-    long long dis = abs(house[u].x-x) + abs(house[u].y-y);
-    if(np >= house[u].w+pw[dis]){
-        // 去一次
-        long long mn = min((np - pw[dis])/house[u].w, house[u].g);
-        long long num = min(mn, nm);
-        res += num;
-        nt--;
-        ans = max(ans, res);
-        if(nt && u != nc){
-            f(u+1);
-        }
-        res -= num;
-        nt++;
-        // 去两次
-        if(nt>1 && num < house[u].g){
-            num = min(2 * num, house[u].g);
-            res += num;
-            nt-=2;
-            ans = max(ans, res);
-            if(nt && u != nc){
-                f(u+1);
-            }
-            res -= num;
-            nt+=2;
-        }
-    }
-    // 不去
-    if(u != nc){
-        f(u+1);
-    }
 }
 
 void read(long long &_x){
