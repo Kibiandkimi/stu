ddf2b0dae359a43dafd74cd265e3d47a453d0af6
Kibi <kibiandkimi@outlook.com>
2024-05-14 20:41:49 +0800

    update P4092 not finish Problem

---
+++ P4092.cpp
@@ -0,0 +1,287 @@
+//
+// Created by Lenovo on 2024/5/14.
+//
+// TODO 24-5-14
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    auto read = [](int &x){
+        int s = 0, w = 1, c = getchar();
+        while(c < '0' || '9' < c){
+            if(c == '-'){
+                w = -1;
+            }
+            c = getchar();
+        }
+        while('0' <= c && c <= '9'){
+            s = s * 10 + c - '0';
+            c = getchar();
+        }
+        x = s * w;
+    };
+
+    class Tree{
+        class Node{
+        public:
+            int id{};
+            Node *fa{};
+            vector<Node*> son;
+        };
+
+        vector<Node> node;
+
+    public:
+        explicit Tree(int n):node(n){
+            for(int i = 0; i < n; i++){
+                node[i].id = i + 1;
+            }
+        }
+
+        void add_edge(int u, int v){
+            node[u - 1].son.emplace_back(&node[v - 1]);
+            node[v - 1].fa = &node[u - 1];
+        }
+
+        Node* get_node(int u){
+            return &node[u - 1];
+        }
+    };
+
+    class SegTree{
+        class Node{
+        public:
+            Node *ls{nullptr}, *rs{nullptr};
+            int l{0}, r{0}, val{0}, dep{0};
+        };
+
+        Node *rt;
+
+        static void update(Node *u){
+            u->val = u->ls->dep > u->rs->dep ? u->ls->val : u->rs->val;
+            u->dep = max(u->ls->dep, u->rs->dep);
+        }
+
+    public:
+        explicit SegTree(int n){
+            rt = new Node;
+            using build = tuple<Node*, int, int, int>; // u, l, r, sta
+            stack<build> stk;
+            stk.emplace(rt, 1, n, 0);
+            while (!stk.empty()){
+                auto [u, l, r, sta] = stk.top();
+                stk.pop();
+                switch(sta){
+                    case 0:
+                        u->l = l, u->r = r, u->val = (l == 1 ? 1 : 0), u->dep = (l == 1 ? 1 : 0);
+                        if(l != r){
+                            stk.emplace(u, l, r, 1);
+                            u->ls = new Node, u->rs = new Node;
+                            stk.emplace(u->ls, l, (l + r) >> 1, 0);
+                            stk.emplace(u->rs, ((l + r) >> 1) + 1, r, 0);
+                        }
+                        break;
+                    case 1:
+                        update(u);
+                        break;
+                    default:
+                        throw runtime_error("Error stage!");
+                }
+            }
+        }
+
+
+        void modify(int u, int dep){
+            stack<Node*> stk;
+            stk.emplace(rt);
+            Node *now = rt;
+            while(now->l != now->r){
+                if(u <= now->ls->r){
+                    now = now->ls;
+                }else{
+                    now = now->rs;
+                }
+                stk.emplace(now);
+            }
+            now->val = u;
+            now->dep = dep;
+            stk.pop();
+            while(!stk.empty()){
+                update(stk.top());
+            }
+        }
+
+        int query(int l, int r){
+            using q = tuple<Node*, int>; // u, stage
+            using Res = tuple<int, int>; // val, dep
+            stack<q> stk;
+            stack<Res> res;
+            stk.emplace(rt, 0);
+            while(!stk.empty()){
+                auto [u, sta] = stk.top();
+                stk.pop();
+                switch(sta){
+                    case 0:
+                        if(l <= u->l && u->r <= r) {
+                            res.emplace(u->val, u->dep);
+                            break;
+                        }
+                        stk.emplace(u, 1);
+                        if(l <= u->ls->r){
+                            stk.emplace(u->ls, 0);
+                        }
+                        if(r >= u->rs->l){
+                            stk.emplace(u->rs, 0);
+                        }
+                        break;
+                    case 1: {
+                        Res temp1{0, 0}, temp2{0, 0};
+                        if (l <= u->ls->r) {
+                            temp1 = res.top();
+                            res.pop();
+                        }
+                        if (r >= u->rs->l) {
+                            temp2 = res.top();
+                            res.pop();
+                        }
+                        auto [v1, d1] = temp1;
+                        auto [v2, d2] = temp2;
+                        res.emplace(d1 > d2 ? v1 : v2, max(d1, d2));
+                    }
+                    break;
+                    default:
+                        throw runtime_error("Error stage!");
+                }
+            }
+
+            return get<0>(res.top());
+        }
+    };
+
+    class HPD{
+        class Node{
+        public:
+            Node *top, *fa, *son;
+            int dfn, id, siz, dep;
+        };
+
+        vector<Node> node;
+
+        SegTree segTree;
+
+    public:
+        HPD(int n, int rt, Tree &tree):node(n), segTree(n){
+            for (int i = 0; i < n; i++){
+                node[i].id = i + 1;
+                node[i].siz = 1;
+            }
+
+            // dfs1
+            using dfs1 = tuple<Node*, Node*, int>; // u, fa, stage
+            stack<dfs1> stk1;
+            stk1.emplace(&node[rt - 1], nullptr, 0);
+            node[rt - 1].dep = 1;
+            while(!stk1.empty()){
+                auto [u, fa, sta] = stk1.top();
+                stk1.pop();
+                switch(sta){
+                    case 0:
+                        stk1.emplace(u, fa, 1);
+                        for(auto &v : tree.get_node(u->id)->son){
+                            node[v->id - 1].fa = u;
+                            node[v->id - 1].dep = u->dep + 1;
+                            stk1.emplace(&node[v->id], u, 0);
+                        }
+                        break;
+                    case 1:
+                        fa ? fa->siz += u->siz : 0;
+                        fa ? fa->son ? fa->son->siz > u->siz ? fa->son : fa->son = u : fa->son = u : nullptr;
+                        break;
+                    default:
+                        throw runtime_error("Error stage!");
+                }
+            }
+
+            // dfs2
+            int cnt = 0;
+            using dfs2 = tuple<Node*, Node*, int>; // u, top, stage
+            stack<dfs2> stk2;
+            stk2.emplace(&node[rt - 1], &node[rt - 1], 0);
+            while (!stk2.empty()){
+                auto [u, top, sta] = stk2.top();
+                stk2.pop();
+                switch(sta){
+                    case 0:
+                        u->top = top;
+                        u->dfn = ++cnt;
+                        stk2.emplace(u, top, 1);
+                        u->son ? stk2.emplace(u->son, top, 0) : void();
+                        break;
+                    case 1:
+                        for (auto &v : tree.get_node(u->id)->son){
+                            if(u->son == nullptr || v->id != u->son->id){
+                                stk2.emplace(&node[v->id - 1], &node[v->id - 1], 0);
+                            }
+                        }
+                        break;
+                    default:
+                        throw runtime_error("Error stage!");
+                }
+            }
+        }
+
+        void modify(int u){
+            segTree.modify(u, node[u - 1].dep);
+        }
+
+        int query(int u){
+            Node *now = &node[u - 1];
+            int res = segTree.query(now->top->dfn, now->dfn);
+            while(res == 0){
+                now = now->top->fa;
+                res = segTree.query(now->top->dfn, now->dfn);
+            }
+            return res;
+        }
+    };
+
+    int n, q;
+    read(n), read(q);
+
+    Tree tree(n);
+
+    for(int i = 1; i < n; i++){
+        int u, v;
+        read(u), read(v);
+        tree.add_edge(u, v);
+    }
+
+    HPD hpd(n, 1, tree);
+
+    bool vis[n + 1];
+    memset(vis, 0, sizeof vis);
+
+    vis[1] = true;
+
+    while(q--){
+        auto readChar = [](int &x){
+            int c = getchar();
+            while(c != 'C' && c != 'Q'){
+                c = getchar();
+            }
+            x = c;
+        };
+
+        int type, u;
+        readChar(type), read(u);
+        if(type == 'C'){
+            if(!vis[u]){
+                vis[u] = true;
+                hpd.modify(u);
+            }
+        }else{
+            printf("%d\n", hpd.query(u));
+        }
+    }
+
+}
